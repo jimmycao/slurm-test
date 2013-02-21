039fefcc2cce651f13c15caeafccbf0906ee127b
jimmycao <jimmy.cao@emc.com>
2013-02-21 14:57:07 +0800

    init

---
+++ src/common/list_test.c
@@ -0,0 +1,19 @@
+/*
+ * list_test.c
+ *
+ *  Created on: Feb 21, 2013
+ *      Author: caoj7
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#include "list.h"
+
+
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
---
+++ src/test/list_test.c
@@ -0,0 +1,8 @@
+/*
+ * list_test.c
+ *
+ *  Created on: Feb 21, 2013
+ *      Author: caoj7
+ */
+
+
